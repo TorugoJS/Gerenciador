6f784968b0f9ac7754c9ca52317a8b688a99f23e
TorugoJS <victor.devfrontend@gmail.com>
2022-06-23 17:42:48 -0300

    commit manager

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import Home from './components/pages/Home';
 import Contato from './components/pages/Contato';
 import NovosProjetos from './components/pages/NovoProjeto';
 import Empresa from './components/pages/Empresa';
+import Projetos from './components/pages/Projetos';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/empresa">Contato</Link>
-        <Link to="/contato">Contato</Link>
-        <Link to="/novosprojetos">NovosProjetos</Link>
-      </div>
+    <Navbar />
 
 
       <Container customClass ="min-height">
@@ -24,10 +23,11 @@ function App() {
           <Route exact path="/" element={<Home />}></Route>
           <Route exact path="/contato" element={<Contato />}></Route>
           <Route exact path="/empresa" element={<Empresa />}></Route>
-          <Route exact path="/novosprojetos" element={<NovosProjetos />}></Route>
+          <Route exact path="/projetos" element={<Projetos />}></Route>
+          {/* <Route exact path="/novosprojetos" element={<NovosProjetos />}></Route> */}
         </Routes>
       </Container>
-      <footer>Footer</footer>
+      <Footer />
     </Router>
 
   );
---
+++ src/components/pages/Projetos.js
@@ -0,0 +1,5 @@
+function Projetos (){
+    return( <h1>Projetos</h1>)
+}
+
+export default Projetos;
