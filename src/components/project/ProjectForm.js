aa96e5aa2ea3fd41bd8f5acb4b6feabbd29470d4
TorugoJS <victor.devfrontend@gmail.com>
2022-07-05 19:23:42 -0300

    Manager

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react'
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}) {
+function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([])
+
+    
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input type="text"
@@ -16,7 +36,8 @@ function ProjectForm({btnText}) {
                 name="budget"
                 placeholder="Insira o orçamento total" />
 
-            <Select name="category-_id" text="Selecione a categoria" />
+            <Select name="category-_id" text="Selecione a categoria"
+            options={categories} />
 
             <SubmitButton text={btnText} />
         </form>
