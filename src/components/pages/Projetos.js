0459efcbf3d00a82488c2afe6b16f15229ad65db
TorugoJS <victor.devfrontend@gmail.com>
2022-07-07 19:38:37 -0300

    manager

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -1,22 +1,29 @@
 import { useLocation } from 'react-router-dom'
 
-import Message from "../layout/Message";
+import Message from "../layout/Message"
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+
+import styles from './Projetos.module.css'
 
 function Projetos() {
 
     const location = useLocation()
     let message = ''
-    if(location.state){
+    if (location.state) {
         message = location.state.message
     }
 
     return (
-        <div>
-            <h1>Meus projetos</h1>
+        <div className={styles.projetos_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/novosprojetos" text= "Criar Projeto" />
+            </div>
             {message && <Message type="success" msg={message} />}
-            
-
-
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
 
 
