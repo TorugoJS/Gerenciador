f77dfb871d35230020e43571bf4a9e64575f1fa6
TorugoJS <victor.devfrontend@gmail.com>
2022-07-07 22:17:56 -0300

    manager

--- src/components/pages/Projetos.js
+++ src/components/pages/Projetos.js
@@ -1,12 +1,17 @@
 import { useLocation } from 'react-router-dom'
 
+import { useState, useEffect } from 'react'
+
 import Message from "../layout/Message"
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
 
 import styles from './Projetos.module.css'
 
 function Projetos() {
+
+    const [projetos, setProjetos] = useState([])
 
     const location = useLocation()
     let message = ''
@@ -14,15 +19,35 @@ function Projetos() {
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setProjetos(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <div className={styles.projetos_container}>
             <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
-                <LinkButton to="/novosprojetos" text= "Criar Projeto" />
+                <LinkButton to="/novosprojetos" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projetos.length > 0 && projetos.map((project) =>
+                    <ProjectCard id={project.id} name={project.name}
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                     />)}
+
             </Container>
         </div>
 
